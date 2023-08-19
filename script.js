f8d0d76686ef9f9458f90eb8ed90d7a7cfd49d0d
sgebre2 <sgebre2@unl.edu>
2023-08-19 00:18:53 -0500

    Finished the draft skeleton of the Projects section. Started on the contact form section.

--- script.js
+++ script.js
@@ -192,6 +192,25 @@ if (buttonId == "Programming") {
 
 };
 
+
+function displayCheck(selectValue) {
+  //Add always none value for projectitem
+  if (selectValue == 'softwarePackages') {
+    const softwarePackageProjectItems = document.querySelectorAll(".softwarePackages");
+    softwarePackageProjectItems.forEach((projectItem) => {
+      projectItem.style.display = "grid";
+    });
+  }
+}
+
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.elements.name.value = '';
+  e.target.elements.email.value = '';
+  e.target.elements.message.value = '';
+});
+
+
 function myFunction() {
   alert("Hello! I am an alert box!!");
 };

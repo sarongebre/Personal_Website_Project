623c6c417a079d59089a6626aa4406b2064caecd
sarongebre <saron.solomon2000@gmail.com>
2023-11-11 15:27:15 -0600

    Worked on nav, contact, and log changes

---
+++ contact.php
@@ -0,0 +1,28 @@
+<!-- TODO: To be configured once I set up my server. For now, I will just use - formspree.io -->
+
+
+<?php
+// Get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+ 
+$to = "yourEmail@gmail.com";
+$subject = "This is the subject line";
+ 
+// The following text will be sent
+// Name = user entered name
+// Email = user entered email
+// Message = user entered message 
+$txt ="Name = ". $name . "\r\n  Email = "
+    . $email . "\r\n Message =" . $message;
+ 
+$headers = "From: noreply@demosite.com" . "\r\n" .
+            "CC: somebodyelse@example.com";
+if($email != NULL) {
+    mail($to, $subject, $txt, $headers);
+}
+ 
+// Redirect to
+header("Location:last.html");
+?>
